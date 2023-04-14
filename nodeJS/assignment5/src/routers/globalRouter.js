ac17472fccf4862baa80581345d9561bc52e7da7
onelife90 <onelife90@naver.com>
2023-04-14 12:44:17 +0900

    Added: home callback

--- nodeJS/assignment5/src/routers/globalRouter.js
+++ nodeJS/assignment5/src/routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
+import { home } from "../controllers/globalController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
-globalRouter.get("/trending", trending);
-globalRouter.get("/new", newest);
-globalRouter.get("/join", join);
-globalRouter.get("/login", login);
+// globalRouter.get("/trending", trending);
+// globalRouter.get("/new", newest);
+// globalRouter.get("/join", join);
+// globalRouter.get("/login", login);
 
 export default globalRouter;
