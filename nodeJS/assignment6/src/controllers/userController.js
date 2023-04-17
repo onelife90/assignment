fe06f21fc25f1a5e75ccb41b8336d44ac5ba525d
onelife90 <onelife90@naver.com>
2023-04-17 13:37:26 +0900

    Added: users template render

--- nodeJS/assignment6/src/controllers/userController.js
+++ nodeJS/assignment6/src/controllers/userController.js
@@ -1,4 +1,6 @@
-export const join = (req, res) => res.send("<h1>Join</h1>");
-export const login = (req, res) => res.send("<h1>Login</h1>");
-export const userId = (req, res) => res.send("<h1>UserID</h1>");
-export const editProfile = (req, res) => res.send("<h1>Edit Profile</h1>");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const userId = (req, res) =>
+  res.render("userId", { pageTitle: "userId", userId: `${req.params.id}` });
+export const editProfile = (req, res) =>
+  res.render("editProfile", { pageTitle: "Edit Profile" });
